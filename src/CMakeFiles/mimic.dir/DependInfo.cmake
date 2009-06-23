9a7c811270a5ce61b33380d77bdca3d7040b5ec4
Lukas Käll <lukas.kall@cbr.su.se>
2009-06-23 16:12:24 +0200

    first commit

---
+++ src/CMakeFiles/mimic.dir/DependInfo.cmake
@@ -0,0 +1,15 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lukask/cvs/percolator/mimic/src/AminoAcidDist.cpp" "/home/lukask/cvs/percolator/mimic/src/CMakeFiles/mimic.dir/AminoAcidDist.cpp.o"
+  "/home/lukask/cvs/percolator/mimic/src/Option.cpp" "/home/lukask/cvs/percolator/mimic/src/CMakeFiles/mimic.dir/Option.cpp.o"
+  "/home/lukask/cvs/percolator/mimic/src/Peptides.cpp" "/home/lukask/cvs/percolator/mimic/src/CMakeFiles/mimic.dir/Peptides.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ src/CMakeFiles/mimic.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/mimic.dir/AminoAcidDist.cpp.o"
+  "CMakeFiles/mimic.dir/Option.cpp.o"
+  "CMakeFiles/mimic.dir/Peptides.cpp.o"
+  "mimic.pdb"
+  "mimic"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/mimic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
